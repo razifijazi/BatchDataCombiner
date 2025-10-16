cb711f5bb7d4967f036c4dcb509258db5a156e81
[dyad] <razifijazi@gmail.com>
2025-10-16 19:30:57 +0700

    [dyad] Creating a link and word combiner tool - wrote 2 file(s), deleted 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,11 @@
 import { MadeWithDyad } from "@/components/made-with-dyad";
-import UrlParser from "@/components/UrlParser";
+import LinkCombiner from "@/components/LinkCombiner";
 
 const Index = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900">
-      <div className="w-full max-w-6xl">
-        <UrlParser />
+      <div className="w-full max-w-4xl">
+        <LinkCombiner />
       </div>
       <div className="absolute bottom-0">
         <MadeWithDyad />
